0f361d6a3d33237207887d3ef11301f43e9bc3ff
Yash Godara <yash17godara@gmail.com>
2024-07-10 15:53:21 +0530

    Update provider.tsx

--- components/provider.tsx
+++ components/provider.tsx
@@ -1,16 +1,16 @@
 "use client"
 
 import * as React from "react"
+import { LangProvider } from "@/context/langContext"
+import { Locale } from "@/i18n.config"
 import { TooltipProvider } from "@/ui/tooltip"
 import { Provider as JotaiProvider } from "jotai"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { ThemeProviderProps } from "next-themes/dist/types"
-import { Locale } from '@/i18n.config'
-import { LangProvider } from "@/context/langContext"
 
 export interface ProviderProps extends ThemeProviderProps {
   children: React.ReactNode
-  params: { lang: any }
+  params: { locale: Locale }
 }
 
 export function Provider({ children, params, ...props }: ProviderProps) {
@@ -18,9 +18,7 @@ export function Provider({ children, params, ...props }: ProviderProps) {
     <JotaiProvider>
       <NextThemesProvider {...props}>
         <TooltipProvider delayDuration={0}>
-          <LangProvider params={params}>
-          {children}
-          </LangProvider>
+          <LangProvider params={params}>{children}</LangProvider>
         </TooltipProvider>
       </NextThemesProvider>
     </JotaiProvider>
