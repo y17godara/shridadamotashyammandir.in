f469c95a051a472a23437654d2b33d9f452684d9
Yash Godara <yash17godara@gmail.com>
2024-07-20 22:31:36 +0530

    styles: Updates

--- app/[locale]/(app)/layout.tsx
+++ app/[locale]/(app)/layout.tsx
@@ -8,20 +8,9 @@ interface AppLayoutProps {
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <>
-        <main className="relative flex-1">
-          <div className="fixed inset-0 z-0 bg-gradient-to-b from-background to-foreground"></div>
-          <div className="relative z-10 min-h-screen flex flex-col">
-            <div className="sticky top-0 bg-transparent">
-              <SiteHeader />
-            </div>
-            <div className="flex-1 bg-transparent min-h-[300vh]">
-              {children}
-            </div>
-            <div className="sticky bottom-0 bg-transparent">
-              <SiteFooter />
-            </div>
-          </div>
-        </main>
-      </>
+      <SiteHeader />
+      <main className="min-h-screen">{children}</main>
+      <SiteFooter />
+    </>
   )
 }
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,16 +2,18 @@ import React from "react"
 import type { Metadata } from "next"
 
 import "./globals.css"
+
+import { i18n, Locale } from "@/i18n.config"
+
 import { fontSans } from "@/config/fonts"
 import { SiteMetadata } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/dev/tailwind-indicator"
 import { Provider } from "@/components/provider"
 import { ThemeSwitcher } from "@/components/theme-switcher"
-import { Locale, i18n } from '@/i18n.config'
 
 export async function generateStaticParams() {
-  return i18n.locales.map(locale => ({ lang: locale }))
+  return i18n.locales.map((locale) => ({ lang: locale }))
 }
 
 export const metadata: Metadata = {
@@ -26,15 +28,11 @@ export default function RootLayout({
   params: { locale: Locale }
 }>) {
   return (
-    <html 
-      lang={params.locale}
-      dir="ltr"
-      suppressHydrationWarning
-    >
+    <html lang={params.locale} dir="ltr" suppressHydrationWarning>
       <head />
       <body
         className={cn(
-          "relative flex flex-col min-h-screen bg-background font-sans antialiased",
+          "relative flex flex-col overflow-x-hidden min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
